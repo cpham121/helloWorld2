dd09b87e0802699deb559a9d0f47bb846e4349bb
cpham121 <cpham121@terpmail.umd.edu>
2026-09-11 16:54:15 -0400

    Configured flask

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Celia Pham! I am adding my first code change'
+    return 'Hello World from Celia Pham! This is my first HTML page. '
 
 @app.route('/hello')
 def hello():
@@ -13,4 +13,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000, debug=True)
