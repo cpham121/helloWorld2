bf7a490bce3c4436d8fb405ba645e7444d5bac6a
cpham121 <cpham121@terpmail.umd.edu>
2026-09-12 12:55:58 -0400

    Updating route for hello html page.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Celia Pham! This is my first HTML page. '
+    return 'Hello World from Celia Pham! I am adding my first code change. '
 
 @app.route('/hello')
 def hello():
